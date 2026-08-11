fcbf603b64fc104a113c03a484e0f448a397bd01
Fabiano1991 <fabiovenaruzzo@gmail.com>
2026-08-11 08:56:34 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,39 @@ self.addEventListener("install", event => {
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(filesToCache))
   );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
+    fetch(event.request)
       .then(response => {
-        return response || fetch(event.request);
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, copy);
+          });
+
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
       })
   );
 });
