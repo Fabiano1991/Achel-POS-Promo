03a3add2dfc4d356549c68afb5d5aa15cfa59e28
Fabiano1991 <fabiovenaruzzo@gmail.com>
2026-08-14 16:37:08 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,23 @@
 const CACHE_NAME =
-  "achel-pos-v13";
+  "achel-pos-v14";
 
 
 const STATIC_FILES = [
+
   "./",
+
   "./index.html",
+
   "./manifest.json",
+
   "./admin.js",
+
   "./wholesale.js",
+
   "./achel-kluis-home.jpg",
+
   "./achel-logo.png"
+
 ];
 
 
@@ -18,17 +26,21 @@ self.addEventListener(
   event => {
 
     event.waitUntil(
+
       caches
         .open(
           CACHE_NAME
         )
+
         .then(
           cache =>
             cache.addAll(
               STATIC_FILES
             )
         )
+
     );
+
 
     self.skipWaiting();
 
@@ -41,26 +53,36 @@ self.addEventListener(
   event => {
 
     event.waitUntil(
+
       caches
         .keys()
+
         .then(
           names =>
+
             Promise.all(
+
               names
+
                 .filter(
                   name =>
                     name !==
                     CACHE_NAME
                 )
+
                 .map(
                   name =>
                     caches.delete(
                       name
                     )
                 )
+
             )
+
         )
+
     );
+
 
     self.clients.claim();
 
@@ -76,7 +98,9 @@ self.addEventListener(
       event.request.method !==
       "GET"
     ) {
+
       return;
+
     }
 
 
@@ -86,35 +110,44 @@ self.addEventListener(
     ) {
 
       event.respondWith(
+
         fetch(
           event.request
         )
+
           .catch(
             () =>
               caches.match(
                 "./index.html"
               )
           )
+
       );
 
+
       return;
+
     }
 
 
     event.respondWith(
+
       fetch(
         event.request
       )
+
         .then(
           response => {
 
             const copy =
               response.clone();
 
+
             caches
               .open(
                 CACHE_NAME
               )
+
               .then(
                 cache =>
                   cache.put(
@@ -123,16 +156,19 @@ self.addEventListener(
                   )
               );
 
+
             return response;
 
           }
         )
+
         .catch(
           () =>
             caches.match(
               event.request
             )
         )
+
     );
 
   }
