8548a723c65c95a4d06392f0f5d8e90e9deb4370
Fabiano1991 <fabiovenaruzzo@gmail.com>
2026-09-08 15:05:07 +0200

    Update beurzen.js

--- beurzen/beurzen.js
+++ beurzen/beurzen.js
@@ -688,7 +688,7 @@ function bindForms() {
     )
     ?.addEventListener(
       "click",
-      exportContactsCsv
+      () => exportContactsCsv()
     );
 
   document
@@ -697,7 +697,7 @@ function bindForms() {
     )
     ?.addEventListener(
       "click",
-      exportOrdersCsv
+      () => exportOrdersCsv()
     );
 }
 
